aa19422a908b9310a054558a9c592a5b5bd0e5fe
adas <ajanta744@gmail.com>
2023-09-21 12:00:27 +1000

    updates

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
 
-        <h1>Welcome to my website</h1>
+        <h1>Welcome to Ajanta website</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ipsum, blanditiis tempore cumque itaque consequatur. Blanditiis soluta sunt nulla eos odio rem distinctio consectetur delectus voluptatum repellendus unde sint dolor, officiis, aliquid, ipsa error architecto veritatis dolorum voluptas rerum omnis excepturi eum dicta ex! Alias, optio explicabo neque temporibus, obcaecati aut dolor officiis, doloremque iste distinctio nemo blanditiis velit deleniti hic facilis laboriosam minus ipsa tenetur qui! Consequatur eaque numquam animi tempore dignissimos qui veniam consectetur labore molestias perferendis autem fuga facilis cupiditate libero, sapiente aliquam magni? Nesciunt 
+          sed excepturi culpa inventore cum fuga distinctio, sequi esse obcaecati assumenda commodi.</p>
        
     
       </header>
